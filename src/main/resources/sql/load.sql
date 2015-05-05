671bad6434672cf4744e7fa5255bc5c59b23a267
semichamza <semichamza@gmail.com>
2015-05-05 14:59:02 +0200

    enitity refactoring

--- src/main/resources/sql/load.sql
+++ src/main/resources/sql/load.sql
@@ -1,24 +1,20 @@
-INSERT INTO `pms`.`user_roles` (`name`) VALUES ('Administrator');
-INSERT INTO `pms`.`user_roles` (`name`) VALUES ('Normal');
-
-INSERT INTO `pms`.`users` (`active`, `firstName`, `lastName`, `passwordHash`, `username`, `user_role_id`) VALUES (TRUE, 'Sinan', 'Saki?', '098f6bcd4621d373cade4e832627b4f6', 'admin', '1');
-INSERT INTO `pms`.`users` (`active`, `firstName`, `lastName`, `passwordHash`, `username`, `user_role_id`) VALUES (TRUE, 'Jasmin', 'Kaldžija', '098f6bcd4621d373cade4e832627b4f6', 'jkaldzija', '2');
-INSERT INTO `pms`.`users` (`active`, `firstName`, `lastName`, `passwordHash`, `username`, `user_role_id`) VALUES (TRUE, 'Alem', 'Zeki?', '098f6bcd4621d373cade4e832627b4f6', 'azekic', '2');
-INSERT INTO `pms`.`users` (`active`, `firstName`, `lastName`, `passwordHash`, `username`, `user_role_id`) VALUES (TRUE, 'Hamza', 'Semi?', '098f6bcd4621d373cade4e832627b4f6', 'hsemic', '2');
-INSERT INTO `pms`.`users` (`active`, `firstName`, `lastName`, `passwordHash`, `username`, `user_role_id`) VALUES (TRUE, 'Almin', '?iva', '098f6bcd4621d373cade4e832627b4f6', 'aciva', '2');
-
-INSERT INTO `pms`.`project_roles` (`name`) VALUES ('Owner');
-INSERT INTO `pms`.`project_roles` (`name`) VALUES ('Member');
+INSERT INTO `pms`.`users` (`active`, `firstName`, `lastName`, `passwordHash`, `username`, `userRole`) VALUES (TRUE, 'Sinan', 'Saki?', '098f6bcd4621d373cade4e832627b4f6', 'admin', 'ADMINISTRATOR');
+INSERT INTO `pms`.`users` (`active`, `firstName`, `lastName`, `passwordHash`, `username`, `userRole`) VALUES (TRUE, 'Jasmin', 'Kaldžija', '098f6bcd4621d373cade4e832627b4f6', 'jkaldzija', 'NORMAL');
+INSERT INTO `pms`.`users` (`active`, `firstName`, `lastName`, `passwordHash`, `username`, `userRole`) VALUES (TRUE, 'Alem', 'Zeki?', '098f6bcd4621d373cade4e832627b4f6', 'azekic', 'NORMAL');
+INSERT INTO `pms`.`users` (`active`, `firstName`, `lastName`, `passwordHash`, `username`, `userRole`) VALUES (TRUE, 'Hamza', 'Semi?', '098f6bcd4621d373cade4e832627b4f6', 'hsemic', 'NORMAL');
+INSERT INTO `pms`.`users` (`active`, `firstName`, `lastName`, `passwordHash`, `username`, `userRole`) VALUES (TRUE, 'Almin', '?iva', '098f6bcd4621d373cade4e832627b4f6', 'aciva', 'NORMAL');
+INSERT INTO `pms`.`users` (`active`, `firstName`, `lastName`, `passwordHash`, `username`, `userRole`) VALUES (TRUE, 'TEST', 'TEST', '098f6bcd4621d373cade4e832627b4f6', 'test', 'NORMAL');
 
 INSERT INTO `pms`.`projects` (`description`, `name`) VALUES ('Testni projekat', 'Hello world');
 INSERT INTO `pms`.`projects` (`description`, `name`) VALUES ('Projekat sa predmeta NWT', 'PMS-NWT');
+INSERT INTO `pms`.`projects` (`description`, `name`) VALUES ('Testni 3', 'Testni 3');
 
-INSERT INTO `pms`.`projects_users` (`user_id`, `project_id`, `project_role_id`) VALUES ('1', '1', '1');
-INSERT INTO `pms`.`projects_users` (`user_id`, `project_id`, `project_role_id`) VALUES ('1', '2', '1');
-INSERT INTO `pms`.`projects_users` (`user_id`, `project_id`, `project_role_id`) VALUES ('2', '2', '2');
-INSERT INTO `pms`.`projects_users` (`user_id`, `project_id`, `project_role_id`) VALUES ('3', '2', '2');
-INSERT INTO `pms`.`projects_users` (`user_id`, `project_id`, `project_role_id`) VALUES ('4', '2', '2');
-INSERT INTO `pms`.`projects_users` (`user_id`, `project_id`, `project_role_id`) VALUES ('5', '2', '2');
+INSERT INTO `pms`.`projects_users` (`user_id`, `project_id`, `projectRole`) VALUES ('1', '1', 'OWNER');
+INSERT INTO `pms`.`projects_users` (`user_id`, `project_id`, `projectRole`) VALUES ('1', '2', 'OWNER');
+INSERT INTO `pms`.`projects_users` (`user_id`, `project_id`, `projectRole`) VALUES ('2', '2', 'OWNER');
+INSERT INTO `pms`.`projects_users` (`user_id`, `project_id`, `projectRole`) VALUES ('3', '2', 'MEMBER');
+INSERT INTO `pms`.`projects_users` (`user_id`, `project_id`, `projectRole`) VALUES ('4', '2', 'MEMBER');
+INSERT INTO `pms`.`projects_users` (`user_id`, `project_id`, `projectRole`) VALUES ('5', '2', 'MEMBER');
 
 INSERT INTO `tasks` (`description`, `name`, `project_id`, `user_id`) VALUES ('Hello world task', 'Hello world', '1', '1');
 INSERT INTO `tasks` (`description`, `name`, `project_id`, `user_id`) VALUES ('MySQL + Hibernate + Java EE7 + Angular', 'Konfiguracija okruženja', '2', '4');
